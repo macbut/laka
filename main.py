67e5c56f257439ced0e9c3579a2445050cc14839
dupa <michowy@vp.pl>
2024-10-02 09:37:08 +0200

    comit5

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-class Organiz:
+class World:
+    def __init__(self,sila,inicjatywa,):
+        self.sila=sila
+        self.inicjatywa=inicjatywa
+
+    def wykonajTrue(self):
+        pass
+
+    def rysujSwiat(self):
+
+
