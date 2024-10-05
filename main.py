1e1cdd58632c91ea183bf3c1be2ba3342fc73827
mjace <michowy@vp.pl>
2024-10-05 18:32:54 +0200

    Tło (pygame działa)

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-class World:
-    def __init__(self, sila, inicjatywa):
-        self.sila = sila
-        self.inicjatywa = inicjatywa
+import time
+import random
+import pygame
+import os
+import math
 
-    def wykonajTrue(self):
-        pass
+clock = pygame.time.Clock()
+pygame.init()
+screen = pygame.display.set_mode((1000,1000))
+i=0
+running=True
+while running:
+    i+=1
+    screen.fill((65, 229, 76))
+    clock.tick(60)
+    for event in pygame.event.get():
+        # zdarzenie zamknięcia programu
+        if event.type == pygame.QUIT:
+            running = False
 
-    def rysujSwiat(self):
-        pass
+    pygame.display.flip()
 
-
-
+pygame.quit()
