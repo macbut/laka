4dbd228139e9b9ec57cb191ea6be23c830407a57
mac <macbut0103@gmail.com>
2024-10-06 12:00:16 +0200

    split screen

--- main.py
+++ main.py
@@ -38,16 +38,36 @@ pygame.mixer.init()
 pygame.mixer.music.load(os.path.join("Main them.mp3"))
 pygame.mixer.music.set_volume(0.17)
 pygame.mixer.music.play(-1)
-width=1000
 height=1000
+width=1500
 gracza_x=0
 gracza_y=0
-clock = pygame.time.Clock()
-pygame.init()
-screen = pygame.display.set_mode((height,width))
-screen.fill((33, 161, 71))
+
+screen = pygame.display.set_mode((width,height))
+#konsola i przyciski
+canvas = pygame.Surface((width, height))
+
+laka = pygame.Rect(0, 0, 1000, height)
+console = pygame.Rect(1000, 0, 500, 900)
+buttons = pygame.Rect(1000,900,500,100)
+
+background = pygame.Surface((1000, height))
+background.fill((33, 161, 71))
+background2 = pygame.Surface((500, 900))
+background2.fill((0,0,0))
+background3 = pygame.Surface((500,100))
+background3.fill((255,255,255))
+
+sub1 = canvas.subsurface(laka)
+sub2 = canvas.subsurface(console)
+sub3 = canvas.subsurface(buttons)
+
+#screen.fill((33, 161, 71))
 Delay=0
 balls=pygame.image.load('New_Piskel.png')
+
+pygame.init()
+clock = pygame.time.Clock()
 
 running=True
 while running:
@@ -72,6 +92,16 @@ while running:
     if keys[pygame.K_DOWN]:
         if gracza_y < 940:
             gracza_y += 20
+
+    screen.blit(sub1, (0, 0))
+    screen.blit(sub2, (1000, 0))
+    screen.blit(sub3, (1000,900))
+    sub1.blit(background, (0, 0))
+    sub2.blit(background2, (0, 0))
+    sub3.blit(background3, (0, 0))
+    pygame.draw.line(sub1, (255,255,255), (1000, 0), (1000, height), 1)
+    pygame.draw.line(sub2, (255,255,255), (0, 0), (0, height), 1)
+
     pygame.display.flip()
     time.sleep(0.05)
 
